c94319a81830fa6ff3d829044b8c69c0951875d0
vagrant <vagrant@localhost.localdomain>
2020-07-22 07:04:05 +0000

    [create] association_tutorial

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,18 @@
+class TweetsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]  # deviseのメソッドで「ログインしていないユーザーをログイン画面に送る」メソッド
+  def new
+    @tweet = Tweet.new # 新規投稿用の空のインスタンス
+  end
+
+  def create
+    # 後で書きます。
+  end
+
+  def index
+    @tweets = Tweet.all
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+end
